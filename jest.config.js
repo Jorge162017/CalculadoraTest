c20b9317b967235103958817b02110058669d40d
Jorge162017 <lop221038@uvg.edu.gt>
2024-05-23 18:31:55 -0600

    actualizado

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,4 @@ module.exports = {
     testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testEnvironment: 'jest-environment-jsdom',
-    transform: {
-      "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
-    },
   };
